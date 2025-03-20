4008ea619532e984929126cbb83c166229aff745
thenogodcom <thenogodcom@gmail.com>
2025-03-20 23:29:36 +0800

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -1,52 +1,33 @@
 # name: discourse_skip_email_verification
-# about: Skip email verification during user registration
-# version: 0.1
+# about: Skip email verification during user registration (Improved based on report)
+# version: 0.2 (Improved version)
 # authors: By INSIINC
 # url: https://github.com/yourusername/discourse_skip_email_verification
 enabled_site_setting :skip_email_verification_enabled
 
 after_initialize do
   if SiteSetting.skip_email_verification_enabled
-    Rails.logger.info("Discourse Skip Email Verification: Email verification skipping is enabled.")
+    Rails.logger.info("Discourse Skip Email Verification (v0.2): Email verification skipping is enabled.")
 
-    # Override the UserActivator class behavior
-    class ::UserActivator
-      alias_method :original_factory, :factory
-
-      def factory
-        if !user.active?
-          Rails.logger.info("Discourse Skip Email Verification: Skipping EmailActivator, using LoginActivator.")
-          LoginActivator
-        elsif SiteSetting.must_approve_users?
-          Rails.logger.info("Discourse Skip Email Verification: Skipping ApprovalActivator, using LoginActivator.")
-          LoginActivator
-        else
-          LoginActivator
-        end
-      end
-    end
-
-    # Make sure users are activated immediately
     module ::DiscourseSkipEmailVerification
       def self.auto_activate_user(user)
         return if user.nil? || user.active?
 
-        Rails.logger.info("Discourse Skip Email Verification: Activating user #{user.id} immediately.")
-        user.active = true
-        user.approved = true
-        user.save!
+        Rails.logger.info("Discourse Skip Email Verification (v0.2): Activating user #{user.id} immediately using user.activate.")
+        user.activate  # 使用 user.activate 方法
+        user.save!     # 再次保存用户，确保激活状态持久化 (虽然 activate 内部可能已经保存，但显式保存更稳妥)
       end
     end
 
-    # Hook into user creation to activate immediately
-    on(:user_created) do |user|
-      Rails.logger.info("Discourse Skip Email Verification: User created, attempting to activate user #{user.id}.")
+    # 使用 after_create_user hook
+    after_create_user do |user, opts|
+      Rails.logger.info("Discourse Skip Email Verification (v0.2): User created (via after_create_user hook), attempting to activate user #{user.id}.")
       DiscourseSkipEmailVerification.auto_activate_user(user)
     end
-  else
-    Rails.logger.info("Discourse Skip Email Verification: Email verification skipping is disabled.")
-  end
 
+  else
+    Rails.logger.info("Discourse Skip Email Verification (v0.2): Email verification skipping is disabled.")
+  end
 end
 
 # Add site settings
