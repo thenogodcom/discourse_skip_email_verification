82667d1a6db7f0d1316939f15c752c3bca4f9158
thenogodcom <thenogodcom@gmail.com>
2025-03-21 18:43:55 +0800

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -1,6 +1,6 @@
 # name: discourse-skip-email-verification
 # about: Skip email verification during user registration (Universal - Direct Signup and Invitations)
-# version: 1.7
+# version: 0.7
 # authors: thenogodcom
 # url: https://github.com/thenogodcom/discourse_skip_email_verification
 
@@ -9,17 +9,14 @@ enabled_site_setting :skip_email_verification_enabled
 after_initialize do
   if SiteSetting.skip_email_verification_enabled
     on(:user_created) do |user|
+      # Create the user as normal
       user.active = true
       user.approved = true if !SiteSetting.must_approve_users?
       user.save!
-    end
 
-    class ::InvitesController # <--- Corrected to class ::InvitesController
-      prepend(Module.new do
-        def send_activation_email(user)
-          # Do nothing - block activation emails for invites
-        end
-      end)
+      # Immediately suspend and then activate the user (admin workaround)
+      user.suspend(reason: "Skipping email verification (plugin)", duration: 0.1.second)
+      user.activate
     end
   end
 end
