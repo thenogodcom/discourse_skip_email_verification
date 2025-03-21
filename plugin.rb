c13f8e52202126b2bb4dcbb3b85a2e26dbd55926
thenogodcom <thenogodcom@gmail.com>
2025-03-21 20:23:30 +0800

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -9,24 +9,11 @@ enabled_site_setting :skip_email_verification_enabled
 after_initialize do
   if SiteSetting.skip_email_verification_enabled
     on(:user_created) do |user|
-      # Create the user as normal
       user.active = true
       user.approved = true if !SiteSetting.must_approve_users?
       user.save!
-
-      # Immediately suspend and then activate the user (admin workaround)
-      # Convert minutes to seconds for duration.  Handle potential nil/invalid values.
-      suspend_duration_minutes = SiteSetting.skip_email_verification_suspend_duration
-      suspend_duration_seconds = if suspend_duration_minutes.is_a?(Numeric) && suspend_duration_minutes >= 0
-                                   suspend_duration_minutes * 60
-                                 else
-                                   60  # Default to 60 seconds (1 minute) if invalid
-                                 end
-      user.suspend(reason: "Skipping email verification (plugin)", duration: suspend_duration_seconds.seconds)
-      user.activate
     end
   end
 end
 
-register_asset "stylesheets/common/skip-email-verification.scss"
 register_asset "config/settings.yml"
